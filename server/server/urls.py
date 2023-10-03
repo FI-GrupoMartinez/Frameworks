b19160d886f088e712973c3c007b9fd8716b0799
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-10-03 12:57:58 -0300

    Instalación de api peliculas en servidor

--- server/server/urls.py
+++ server/server/urls.py
@@ -5,6 +5,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from usuarios.api.router import router_user
+from peliculas.api.router import router_peliculas
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -26,4 +27,5 @@ urlpatterns = [
          cache_timeout=0), name='schema-redoc'),  # Documentación minimalista
     path('api/', include('usuarios.api.router')),
     path('api/', include(router_user.urls)),
+    path('api/', include(router_peliculas.urls)),
 ]

070a369452f51442ffb0d74914da0758ebd6643c
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-10-03 12:46:33 -0300

    Configuracion servidor para soporte y almacenamiento de archivos

--- server/server/settings.py
+++ server/server/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import datetime
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/uploads/'  # DEFINE RUTA PARA SUBIR LOST MEDIA
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

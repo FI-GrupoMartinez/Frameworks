7a87bc9faea3073ac446e2a88fe2811b85b2e9e2
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-10-03 12:46:12 -0300

    modelo Pelicula

--- server/peliculas/models.py
+++ server/peliculas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Pelicula(models.Model):
+    nombre = models.CharField(max_length=100)
+    duracion = models.IntegerField()
+    poster = models.ImageField(upload_to='poster_peliculas')
+    video_trailer = models.CharField(max_length=100)
+    clasificacion = models.CharField(max_length=100)
+    actores = models.CharField(max_length=250)
+    director = models.CharField(max_length=100)
+    genero = models.CharField(max_length=100)
+    origen = models.CharField(max_length=100)
+    distribuidor = models.CharField(max_length=100)
+    descripcion_corta = models.CharField(max_length=250)
+    descripcion_larga = models.CharField(max_length=500)
+    tipo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
